745e2192b18d019994b1444ae92b4060d8e2c8fa
Denis Gathondu <thundoss@gmail.com>
2017-01-10 12:25:43 +0300

    Added logic to get input, check for strings and floats, round floats and tests to the functions. Added .gitignore

--- fibo.py
+++ fibo.py
@@ -1,6 +1,39 @@
-# prompt user for input
-# confirm that the number is an interger and not string or float
-# if float, round float input to nearest 10
-# generate fibonacci sequence to the nth number
-# print out the results
-# prompt user to quit or generate another sequence
+def roundFloat(n):
+    return round(n)
+
+
+def checkFloat(n):
+    if isinstance(n, float):
+        return True
+
+
+def checkString(n):
+    try:
+        int(n)
+    except ValueError:
+        print("Value must be a number and not a string or character")
+        return True
+
+
+def getInput(n=None):
+    n = input("Enter a number to generate the fibonacci sequence"
+              "to that number: ")
+
+    return n
+
+
+def fibo(n=None):
+    # prompt user for input
+    n = n or getInput()
+
+    # confirm that the number is an interger and not string or float
+    if checkString(n):
+        n = getInput()
+
+    # if float, round float input to nearest 10
+    if checkFloat():
+        n = roundFloat(n)
+
+    # generate fibonacci sequence to the nth number
+    # print out the results
+    # prompt user to quit or generate another sequence
--- fibo_test.py
+++ fibo_test.py
@@ -1,5 +1,22 @@
+import pytest
+
+from fibo import *
+
+
 # test the user's input is not a string
-# test that a value error is thrown if its a string
+def test_string_input():
+    assertTrue = checkString('fibo')
+
+
+# check that float input is correctly identified
+def test_float_input():
+    assertTrue = checkFloat(5.3)
+
+
 # test that a float input is rounded to nearest 10
+def test_float_input_is_rounded():
+    assert 5 == roundFloat(5.3)
+
+
 # test that the fibonacci sequence is up to nth number
 # test that fibonacci sequence is correct
