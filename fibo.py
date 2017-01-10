7526fd2e187f3b2118d257f7dc7129540a0c7ccc
Denis Gathondu <thundoss@gmail.com>
2017-01-10 11:48:27 +0300

    Wrote pseudocode for generating the sequence and also for the various tests

---
+++ fibo.py
@@ -0,0 +1,6 @@
+# prompt user for input
+# confirm that the number is an interger and not string or float
+# if float, round float input to nearest 10
+# generate fibonacci sequence to the nth number
+# print out the results
+# prompt user to quit or generate another sequence
---
+++ fibo_test.py
@@ -0,0 +1,5 @@
+# test the user's input is not a string
+# test that a value error is thrown if its a string
+# test that a float input is rounded to nearest 10
+# test that the fibonacci sequence is up to nth number
+# test that fibonacci sequence is correct
